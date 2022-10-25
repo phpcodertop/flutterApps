7f9bcb7fa25c62bc8d53c1b1e5d75df6b455eb10
Ahmed Maher Halima <phpcodertop@gmail.com>
2022-10-25 23:17:33 +0200

    Add Ui project and finish slider

---
+++ ui/android/app/src/main/kotlin/com/ahmedhalima/ui/ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ahmedhalima.ui.ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ui/lib/image_carousel.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+class ImageCarousel extends StatefulWidget {
+  const ImageCarousel({Key? key}) : super(key: key);
+
+  @override
+  State<ImageCarousel> createState() => _ImageCarouselState();
+}
+
+class _ImageCarouselState extends State<ImageCarousel> {
+  int photoIndex = 0;
+
+  List<String> photos = [
+    'assets/burger1.jpg',
+    'assets/burger2.jpg',
+    'assets/burger3.jpg',
+    'assets/burger4.jpg',
+  ];
+
+  void _previousImage() {
+    setState(() {
+      photoIndex = photoIndex > 0 ? photoIndex - 1 : 0;
+    });
+  }
+
+  void _nextImage() {
+    setState(() {
+      photoIndex = photoIndex < photos.length - 1 ? photoIndex + 1 : photoIndex;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Image Carousel'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25.0),
+                    image: DecorationImage(
+                      image: AssetImage(photos[photoIndex]),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  height: 400.0,
+                  width: 400.0,
+                ),
+                Positioned(
+                  top: 375.0,
+                  left: 25.0,
+                  right: 25.0,
+                  child: CarouselPoints(
+                    numberOfDots: photos.length,
+                    photoIndex: photoIndex,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => _previousImage(),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green),
+                ),
+                child: const Text('Previous'),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              ElevatedButton(
+                onPressed: () => _nextImage(),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.deepOrange),
+                ),
+                child: const Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CarouselPoints extends StatelessWidget {
+  final int numberOfDots;
+  final int photoIndex;
+
+  const CarouselPoints(
+      {super.key, required this.photoIndex, required this.numberOfDots});
+
+  Widget _inactivePoint() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 3.0, right: 3.0),
+      child: Container(
+        height: 8.0,
+        width: 8.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4.0),
+          color: Colors.grey,
+        ),
+      ),
+    );
+  }
+
+  Widget _activePoint() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 3.0, right: 3.0),
+      child: Container(
+        height: 10.0,
+        width: 10.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4.0),
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                  color: Colors.grey, spreadRadius: 0.0, blurRadius: 2.0),
+            ]),
+      ),
+    );
+  }
+
+  List<Widget>  _buildDots() {
+    List<Widget> dots = [];
+    for(int i = 0; i < numberOfDots; ++i) {
+      dots.add(
+        i == photoIndex ? _activePoint() : _inactivePoint()
+      );
+    }
+    return dots;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: _buildDots(),
+      ),
+    );
+  }
+}
